5d78b0cf076dd74f1d117012290b3c7353b53b03
irailtonreis <irailton.oliveira91@gmail.com>
2023-07-30 17:57:33 -0300

    feat: Lógica da aplicação - Funcionalidade adicionar task

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./global.css";
 import { Header } from "./components/Header";
 import { CheckCircle, Circle, PlusCircle, Trash } from "@phosphor-icons/react";
 
+interface Task {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
 function App() {
+  const [tasks, setTasks] = useState<Task[]>();
+  const [newTask, setNewTask] = useState("");
+  console.log("Task", uuidv4())
+  const addTask = () => {
+    const currentTask = {
+      id: uuidv4(),
+      title: newTask,
+      completed: true,
+    };
+
+    if (tasks?.length) {
+      setTasks([...tasks, currentTask]);
+      setNewTask("");
+    } else {
+      setTasks([currentTask]);
+    }
+  };
+
   return (
     <>
       <div className="container-fluid">
         <Header />
         <div className="container">
           <div className="newTask">
-            <input type="text" placeholder="Adicione uma nova tarefa" />
-            <button>
+            <input
+              type="text"
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+              placeholder="Adicione uma nova tarefa"
+            />
+            <button onClick={addTask}>
               Criar <PlusCircle size={16} className="plusClircle" />
             </button>
           </div>
@@ -24,46 +55,18 @@ function App() {
           </div>
           <div className="listTask">
             <ul>
-              <li className="task">
-                <Circle className="circle" size={24} />{" "}
-                <p className="nameTask">
-                  Integer urna interdum massa libero auctor neque turpis turpis
-                  semper. Duis vel sed fames integer.
-                </p>{" "}
-                <Trash className="trash" size={24} />
-              </li>
-              <li className="task">
-                <Circle className="circle" size={24} />{" "}
-                <p className="nameTask">
-                  Integer urna interdum massa libero auctor neque turpis turpis
-                  semper. Duis vel sed fames integer.
-                </p>{" "}
-                <Trash className="trash" size={24} />
-              </li>
-              <li className="task">
-                <Circle className="circle" size={24} />{" "}
-                <p className="nameTask">
-                  Integer urna interdum massa libero auctor neque turpis turpis
-                  semper. Duis vel sed fames integer.
-                </p>{" "}
-                <Trash className="trash" size={24} />
-              </li>
-              <li className="task">
-                <CheckCircle className="circleCheck" size={24} />{" "}
-                <p className="nameTask">
-                  Integer urna interdum massa libero auctor neque turpis turpis
-                  semper. Duis vel sed fames integer.
-                </p>{" "}
-                <Trash className="trash" size={24} />
-              </li>
-              <li className="task">
-                <CheckCircle className="circleCheck" size={24} />{" "}
-                <p className="nameTask">
-                  Integer urna interdum massa libero auctor neque turpis turpis
-                  semper. Duis vel sed fames integer.
-                </p>{" "}
-                <Trash className="trash" size={24} />
-              </li>
+              {tasks?.map((task) => (
+                <li key={task.id} className="task">
+                  {task.completed ? (
+                    <CheckCircle className="circleCheck" size={24} />
+                  ) : (
+                    <Circle className="circle" size={24} />
+                  )}
+                  <span></span>
+                  <p className="nameTask">{task.title}</p>
+                  <Trash className="trash" size={24} />
+                </li>
+              ))}
             </ul>
           </div>
         </div>
